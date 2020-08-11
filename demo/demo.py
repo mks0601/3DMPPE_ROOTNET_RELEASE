e94b1748a2d0c82d53ee4dcdc974c4f1054b21ea
mks0601 <mks0601@gmail.com>
2020-08-11 23:07:45 +0900

    demo can handle multiple persons

--- demo/demo.py
+++ demo/demo.py
@@ -15,7 +15,7 @@ sys.path.insert(0, osp.join('..', 'data'))
 sys.path.insert(0, osp.join('..', 'common'))
 from config import cfg
 from model import get_pose_net
-from utils.pose_utils import process_bbox, pixel2cam
+from utils.pose_utils import process_bbox
 from dataset import generate_patch_image
 
 def parse_args():
@@ -26,7 +26,7 @@ def parse_args():
 
     # test gpus
     if not args.gpu_ids:
-        assert 0, print("Please set proper gpu ids")
+        assert 0, "Please set proper gpu ids"
 
     if '-' in args.gpu_ids:
         gpus = args.gpu_ids.split('-')
@@ -55,53 +55,49 @@ model.eval()
 # prepare input image
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=cfg.pixel_mean, std=cfg.pixel_std)])
 img_path = 'input.jpg'
-img = cv2.imread(img_path)
-original_height, original_width = img.shape[:2]
+original_img = cv2.imread(img_path)
+original_img_height, original_img_width = original_img.shape[:2]
 
-# prepare bbox and camera intrincis
-bbox = [164, 93, 222, 252] # xmin, ymin, width, height
-bbox = process_bbox(bbox, img.shape[1], img.shape[0])
-assert len(bbox) == 4, 'Please set bbox'
+# prepare bbox for each human
+bbox_list = [
+[139.41, 102.25, 222.39, 241.57],\
+[287.17, 61.52, 74.88, 165.61],\
+[540.04, 48.81, 99.96, 223.36],\
+[372.58, 170.84, 266.63, 217.19],\
+[0.5, 43.74, 90.1, 220.09]] # xmin, ymin, width, height
+person_num = len(bbox_list)
+
+# normalized camera intrinsics
 focal = [1500, 1500] # x-axis, y-axis
-assert len(focal) == 2, 'Please set focal length'
-princpt = [img.shape[1]/2, img.shape[0]/2] # x-axis, y-axis
-assert len(princpt) == 2, 'Please set princpt'
-img, img2bb_trans = generate_patch_image(img, bbox, False, 0.0) 
-img = transform(img).cuda()[None,:,:,:]
-k_value = np.array([math.sqrt(cfg.bbox_real[0]*cfg.bbox_real[1]*focal[0]*focal[1]/(bbox[2]*bbox[3]))]).astype(np.float32)
-k_value = torch.FloatTensor([k_value]).cuda()[None,:]
+princpt = [original_img_width/2, original_img_height/2] # x-axis, y-axis
+print('focal length: (' + str(focal[0]) + ', ' + str(focal[1]) + ')')
+print('principal points: (' + str(princpt[0]) + ', ' + str(princpt[1]) + ')')
 
-# forward
-with torch.no_grad():
-    root_3d = model(img, k_value) # x,y: pixel, z: root-relative depth (mm)
-img = img[0].cpu().numpy()
-root_3d = root_3d[0].cpu().numpy()
+# for cropped and resized human image, forward it to RootNet
+for n in range(person_num):
+    bbox = process_bbox(np.array(bbox_list[n]), original_img_width, original_img_height)
+    img, img2bb_trans = generate_patch_image(original_img, bbox, False, 0.0) 
+    img = transform(img).cuda()[None,:,:,:]
+    k_value = np.array([math.sqrt(cfg.bbox_real[0]*cfg.bbox_real[1]*focal[0]*focal[1]/(bbox[2]*bbox[3]))]).astype(np.float32)
+    k_value = torch.FloatTensor([k_value]).cuda()[None,:]
 
-# save output in 2D space (x,y: pixel)
-vis_img = img.copy()
-vis_img = vis_img * np.array(cfg.pixel_std).reshape(3,1,1) + np.array(cfg.pixel_mean).reshape(3,1,1)
-vis_img = vis_img.astype(np.uint8)
-vis_img = vis_img[::-1, :, :]
-vis_img = np.transpose(vis_img,(1,2,0)).copy()
-vis_root = np.zeros((2))
-vis_root[0] = root_3d[0] / cfg.output_shape[1] * cfg.input_shape[1]
-vis_root[1] = root_3d[1] / cfg.output_shape[0] * cfg.input_shape[0]
-cv2.circle(vis_img, (int(vis_root[0]), int(vis_root[1])), radius=5, color=(0,255,0), thickness=-1, lineType=cv2.LINE_AA)
-cv2.imwrite('output_root_2d.jpg', vis_img)
+    # forward
+    with torch.no_grad():
+        root_3d = model(img, k_value) # x,y: pixel, z: root-relative depth (mm)
+    img = img[0].cpu().numpy()
+    root_3d = root_3d[0].cpu().numpy()
 
-print('Depth from camera: ' + str(root_3d[2]) + ' mm') 
+    # save output in 2D space (x,y: pixel)
+    vis_img = img.copy()
+    vis_img = vis_img * np.array(cfg.pixel_std).reshape(3,1,1) + np.array(cfg.pixel_mean).reshape(3,1,1)
+    vis_img = vis_img.astype(np.uint8)
+    vis_img = vis_img[::-1, :, :]
+    vis_img = np.transpose(vis_img,(1,2,0)).copy()
+    vis_root = np.zeros((2))
+    vis_root[0] = root_3d[0] / cfg.output_shape[1] * cfg.input_shape[1]
+    vis_root[1] = root_3d[1] / cfg.output_shape[0] * cfg.input_shape[0]
+    cv2.circle(vis_img, (int(vis_root[0]), int(vis_root[1])), radius=5, color=(0,255,0), thickness=-1, lineType=cv2.LINE_AA)
+    cv2.imwrite('output_root_2d_' + str(n) + '.jpg', vis_img)
+    
+    print('Root joint depth: ' + str(root_3d[2]) + ' mm')
 
-# camera back-projection
-focal = (None, None) # focal length of x-axis, y-axis. please provide this. if do not know, set normalized one (1500, 1500).
-princpt = (None, None) # princical point of x-axis, y-aixs. please provide this. if do not know, set (original_width/2, original_height/2).
-assert None not in focal, 'pleaset set focal length'
-assert None not in princpt, 'please set princpt'
-# inverse affine transform (restore the crop and resize)
-root_3d[0] = root_3d[0] / cfg.output_shape[1] * cfg.input_shape[1]
-root_3d[1] = root_3d[1] / cfg.output_shape[0] * cfg.input_shape[0]
-root_3d_xy1 = np.concatenate((root_3d[:2], np.ones_like(root_3d[:1])))
-img2bb_trans_001 = np.concatenate((img2bb_trans, np.array([0,0,1]).reshape(1,3)))
-root_3d[:2] = np.dot(np.linalg.inv(img2bb_trans_001), root_3d_xy1)[:2]
-root_3d = pixel2cam(root_3d[None,:], focal, princpt)
-
-print('Root joint position in 3D space (camera-centered, milimeter): ', root_3d.tolist())
